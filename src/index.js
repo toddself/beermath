88a7fa77c42d1effb267fd0e746bc749067cc143
Todd Kennedy <todd@selfassembled.org>
2015-11-15 11:49:15 -0800

    exporting gravity

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import garetz from './bitterness/garetz'
 import strikeTemp from './mash/strike-temp'
 import infusion from './mash/infusion'
 import totalWater from './mash/total-water'
+import fg from './gravity/fg'
+import og from './gravity/og'
+import abv from './gravity/abv'
 
-export {rager, tinseth, garetz, strikeTemp, infusion, totalWater}
+export {rager, tinseth, garetz, strikeTemp, infusion, totalWater, fg, og, abv}
